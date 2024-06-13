571058aa7a350e8507a105d26267a499a40f2773
Nolankicks <nolandimensions@gmail.com>
2024-06-13 16:31:51 -0400

    Add local mode

--- code/Network.cs
+++ code/Network.cs
@@ -4,11 +4,28 @@ using Sandbox.Network;
 public sealed class Network : Component, Component.INetworkListener
 {
 	[Property] public GameObject Player { get; set; }
+	[Property] public bool StartServer { get; set; } = true;
 	protected override void OnAwake()
 	{
-		if (!GameNetworkSystem.IsActive)
+		StartServer = FileSystem.Data.ReadAllText("startserver.txt").ToBool();
+		if (!GameNetworkSystem.IsActive && StartServer)
 		{
 			GameNetworkSystem.CreateLobby();
+		}
+		else if (!StartServer)
+		{
+			var spawns = Scene.GetAllComponents<SpawnPoint>().ToList();
+			if (spawns.Count == 0)
+			{
+				var player = Player.Clone(Vector3.Zero);
+				player.Name = "Local Player";
+			}
+			else
+			{
+				var spawn = Game.Random.FromList(spawns);
+				var player = Player.Clone(spawn.Transform.Position);
+				player.Name = "Local Player";
+			}
 		}
 	}
 
