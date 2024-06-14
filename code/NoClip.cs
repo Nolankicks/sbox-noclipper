70fac510b7bd6738f5871a6831e474065440342f
Nolankicks <nolandimensions@gmail.com>
2024-06-13 22:59:25 -0400

    feat: Add toggle for NoClip mode and improve movement controls

--- code/NoClip.cs
+++ code/NoClip.cs
@@ -10,8 +10,10 @@ public sealed class NoClip : Component
 	[Property, Sync] public int DistanceFromCamera { get; set; } = 200;
 	[Property] public CitizenAnimationHelper AnimationHelper { get; set; }
 	[Property] public float RunSpeed { get; set; } = 2000;
+	[Property, Sync] public bool NoClipping { get; set; } = true;
+	[Property] public CharacterController Controller { get; set; }
 
-	public float GetSpeed()
+	public float GetNoClipSpeed()
 	{
 		if (Input.Down("run"))
 		{
@@ -20,6 +22,17 @@ public sealed class NoClip : Component
 		else
 		{
 			return MoveSpeed;
+		}
+	}
+	public float GetSpeed()
+	{
+		if (Input.Down("run"))
+		{
+			return 350;
+		}
+		else
+		{
+			return 200;
 		}
 	}
 	protected override void OnStart()
@@ -32,16 +45,13 @@ public sealed class NoClip : Component
 		}
 	}
 
-	public void GetFirstPerson()
-	{
-		
-	}
 
 	protected override void OnUpdate()
 	{
 		BodyVis();
 		if (!IsProxy)
 		{
+			Controller.Enabled = !NoClipping;
 			BuildEyeAngles();
 			Camera();
 			if (Input.Pressed("3rd/1st Person Toggle"))
@@ -49,19 +59,86 @@ public sealed class NoClip : Component
 				FirstPerson = !FirstPerson;
 				FileSystem.Data.WriteAllText("firstperson.txt", FirstPerson.ToString());
 			}
+			if (Input.Pressed("Toggle Noclip"))
+			{
+				ToggleNoClip();
+			}
+			if (Input.Down("jump"))
+			{
+				Jump();
+			}
 		}
 	}
-
+	float Friction()
+	{
+		if ( Controller.IsOnGround ) return 6.0f;
+		return 0.2f;
+	}
 	protected override void OnFixedUpdate()
 	{
 		Anims();
 		AnimationHelper.Target.Transform.Rotation = Rotation.Slerp(AnimationHelper.Target.Transform.Rotation, new Angles(0, EyeAngles.yaw, 0).ToRotation(), Time.Delta * 10);
 		if (!IsProxy)
 		{
-			Move();
+			if (!NoClipping)
+			{
+				Move();
+			}
+			else
+			{
+				NoClipMove();
+			}
 		}
 	}
+	public void Jump()
+	{
+		if (Controller is null) return;
+		if (Controller.IsOnGround)
+		{
+			Controller.Punch(Vector3.Up * 300);
+			AnimationHelper.TriggerJump();
+		}
+	}
+	public void Move()
+	{
+		if (Controller is null) return;
+		var cc = Controller;
+		var halfGrav = Scene.PhysicsWorld.Gravity * 0.5f * Time.Delta;
+		WishVelocity = Input.AnalogMove.Normal;
+		if (!WishVelocity.IsNearlyZero())
+		{
+			WishVelocity = new Angles(0, EyeAngles.yaw, 0).ToRotation() * Input.AnalogMove.Normal;
+			WishVelocity *= GetSpeed();
+			WishVelocity = WishVelocity.WithZ(0);
+		}
+		if (!cc.IsOnGround)
+		{
+			WishVelocity.ClampLength(50);
+		}
+		cc.ApplyFriction(Friction());
 
+		if (cc.IsOnGround)
+		{
+			cc.Accelerate(WishVelocity);
+			cc.Velocity = Controller.Velocity.WithZ(0);
+		}
+		else
+		{
+			cc.Accelerate(WishVelocity);
+			cc.Velocity += halfGrav;
+		}
+		cc.Move();
+		if (!cc.IsOnGround)
+		{
+			
+			cc.Velocity += halfGrav;
+		}
+		else
+		{
+			cc.Velocity = cc.Velocity.WithZ(0);
+		}
+
+	}
 	public void BuildEyeAngles()
 	{
 		var ee = EyeAngles;
@@ -70,11 +147,15 @@ public sealed class NoClip : Component
 		ee.roll = 0;
 		EyeAngles = ee;
 	}
-
-	public void Move()
+	void ToggleNoClip()
+	{
+		NoClipping = !NoClipping;
+		WishVelocity = Vector3.Zero;
+	}
+	public void NoClipMove()
 	{
 		WishVelocity = new Angles(EyeAngles.pitch, EyeAngles.yaw, 0).ToRotation() * Input.AnalogMove.Normal;
-		WishVelocity *= GetSpeed();
+		WishVelocity *= GetNoClipSpeed();
 		if (!WishVelocity.IsNearlyZero())
 		{
 			Transform.Position += WishVelocity * Time.Delta;
@@ -126,8 +207,16 @@ public sealed class NoClip : Component
 
 	public void Anims()
 	{
-		AnimationHelper.WithVelocity(WishVelocity);
+		if (NoClipping)
+		{
+			AnimationHelper.WithVelocity(WishVelocity);
+		}
+		else
+		{
+			AnimationHelper.WithVelocity(Controller.Velocity);
+			AnimationHelper.IsGrounded = Controller.IsOnGround;
+		}
 		AnimationHelper.WithWishVelocity(WishVelocity);
-		AnimationHelper.IsNoclipping = true;
+		AnimationHelper.IsNoclipping = NoClipping;
 	}
 }
